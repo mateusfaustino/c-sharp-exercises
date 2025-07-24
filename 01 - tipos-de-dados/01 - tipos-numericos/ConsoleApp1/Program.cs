fc3ec018a4347720b234ccc95bf89196ed395997
mateus <mateusfaustino2024@gmail.com>
2025-07-24 10:52:17 -0300

    exercice tipos-de-dados.tipos-numericos

---
+++ 01 - tipos-de-dados/01 - tipos-numericos/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace tiposNumericos;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Definición de variables
+        int idade = 25;
+        long populacaoMundial = 8_000_000_000;
+        float altura = 1.75f;
+        double pi = 3.14159;
+        decimal precoProduto = 199.99m;
+
+        // Imprimir valores
+        Console.WriteLine($"Idade: {idade}");
+        Console.WriteLine($"População Mundial: {populacaoMundial}");
+        Console.WriteLine($"Altura: {altura}");
+        Console.WriteLine($"Valor de Pi: {pi}");
+        Console.WriteLine($"Preço do Produto: {precoProduto}");
+    }
+}
